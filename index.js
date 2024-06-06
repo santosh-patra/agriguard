4614b133e636708bdf6da3c60e051ad203939c80
santosh-patra <patrasantoshkl@gmail.com>
2024-06-06 17:57:35 +0530

    changes

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import app from "./server/app.js";
 import sequelize from './config/mysqlconfig.js';
 const PORT = 8080;
 
-sequelize.sync()
-    .then(() => {
+// sequelize.sync()
+    // .then(() => {
         app.listen(PORT,()=>{
             console.log("Database & tables Synced!");
             console.log(`Server is running on PORT ${PORT}`);
         })
-    })
-    .catch(err => console.log(err));
+    // })
+    // .catch(err => console.log(err));
 
